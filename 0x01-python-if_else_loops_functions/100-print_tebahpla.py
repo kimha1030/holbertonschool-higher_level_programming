19da98bf17a51111e5beb476754d7df1e2fde691
kimha1030 <1478@holbertonschool.com>
2020-04-28 23:05:14 -0500

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,8 @@
 aux = 1
 for i in range(122, 96, -1):
     if (aux == -1):
-        m = (i - 32)
+        May = (i - 32)
     else:
-        m = i
-    print("{:c}".format(i), end='')
+        May = i
+    print("{:c}".format(May), end='')
     aux = -aux
