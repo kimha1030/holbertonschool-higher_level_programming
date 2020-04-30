c663fe0e51daacaded170eff7bce02644c08dc59
kimha1030 <1478@holbertonschool.com>
2020-04-30 18:15:38 -0500

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for s in dir(hidden_4):
-        if (s[0] != "__"):
-            print(hidden_4)
+        if (s[0] != "_" and s[1] != "_"):
+            print(s)
