b84c64fd50f2a44b772f1020cef2fafc565d8bce
kimha1030 <1478@holbertonschool.com>
2020-04-30 21:58:17 -0500

    Task advanced

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from fprint import print_str
+    print_str()
---
+++ 0x02-python-import_modules/fprint.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import sys
+def print_str():
+    print("#pythoniscool")
