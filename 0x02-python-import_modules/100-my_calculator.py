5db59a6b22fc731c21889c060af7a8cb155d5f5d
kimha1030 <1478@holbertonschool.com>
2020-04-30 19:55:53 -0500

    Advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
     from sys import argv
     from calculator_1 import add, sub, mul, div
     argc = (len(argv) - 1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
     if (argc == 3):
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
         if ((op != '+') and (op != '-') and (op != '*') and (op != '/')):
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
