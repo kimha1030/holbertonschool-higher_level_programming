f4c16e91c09b2c4946b55819d38834e520d5254a
kimha1030 <1478@holbertonschool.com>
2020-04-30 23:30:46 -0500

    Advanced

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import add from magic_calculation_102
+def magic_calculation(a, b):
+    if (a < b):
+        c = add(a, b)
+        for i in range(4, 6)
+            c = c + add
+    else
+    c = sub(a, b)
+    return(c)
+return (0)
