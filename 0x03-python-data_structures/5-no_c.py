d210da2ee32a80eddb3750f5c1a6ff6e69271c94
kimha1030 <1478@holbertonschool.com>
2020-05-05 17:03:52 -0500

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        my_string = list(my_string)
+        new = list()
+        for ch in my_string:
+            if ch != 'c' and ch != 'C':
+                new.append(ch)
+        my_string = ''.join(new)
+        return (my_string)
