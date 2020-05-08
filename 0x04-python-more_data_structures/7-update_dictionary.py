044f5c8260961ac9c080cb8e7e61e871751d39d1
kimha1030 <1478@holbertonschool.com>
2020-05-07 19:04:56 -0500

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary:
-        if i != key in a_dictionary:
-            a_dictionary[key] = [a_dictionary[key], value]
-        else:
-            a_dictionary[key] = value
-            return(a_dictionary)
+    if a_dictionary:
+        for i in a_dictionary:
+            if i != key in a_dictionary:
+                a_dictionary[key] = [a_dictionary[key], value]
+            else:
+                a_dictionary[key] = value
+                return(a_dictionary)
