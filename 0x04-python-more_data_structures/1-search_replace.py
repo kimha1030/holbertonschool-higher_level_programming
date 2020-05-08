6dfaec33838a15e85886770e08f9f9dff9989c39
kimha1030 <1478@holbertonschool.com>
2020-05-08 10:40:16 -0500

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,6 @@
 def search_replace(my_list, search, replace):
-    if len(my_list) == 0:
-        return my_list
-    else:
-        new_list = my_list.copy()
-        for n, e in new_list:
-            if e == search:
-                new_list[n] = replace
-        return (new_list)
+    new_list = my_list.copy()
+    for e in new_list:
+        if e == search:
+            new_list[new_list.index(e)] = replace
+    return (new_list)
