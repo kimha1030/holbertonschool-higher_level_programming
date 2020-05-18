075a81f8a1a072ee660afcaa7f824dcb0f482b3c
kimha1030 <1478@holbertonschool.com>
2020-05-18 14:50:20 -0500

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if int(value):
+            print("{:d}".format(value, end="\n"))
+            return True
+    except ValueError:
+        return False
