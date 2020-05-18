3d0b8a4997dcd6c5eb0a3e8477d6de0fce6e5b11
kimha1030 <1478@holbertonschool.com>
2020-05-18 14:53:52 -0500

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         if int(value):
             print("{:d}".format(value, end="\n"))
-            return True
+        return True
     except ValueError:
         return False
