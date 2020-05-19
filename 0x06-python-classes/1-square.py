29ae4338ca02327c9e7d532d44722734a58ab5dd
kimha1030 <1478@holbertonschool.com>
2020-05-19 16:53:54 -0500

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class size"""
+
+
+class Square:
+    """Class square that define un square"""
+    def __init__(self, size):
+        """Class methods"""
+        self.__size = size
