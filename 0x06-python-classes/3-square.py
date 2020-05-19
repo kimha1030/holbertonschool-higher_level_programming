40fd2622d896094fda46ab4883fada25b75d62df
kimha1030 <1478@holbertonschool.com>
2020-05-19 17:03:17 -0500

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class size"""
+
+
+class Square:
+    """Class square that define un square with validation"""
+    def __init__(self, size=0):
+        """Class methods"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
