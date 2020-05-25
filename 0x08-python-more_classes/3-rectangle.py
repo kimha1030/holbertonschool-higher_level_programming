4062dcf18f2cdfd05f7f9eb48b587d8f1f358718
kimha1030 <1478@holbertonschool.com>
2020-05-25 18:51:40 -0500

    Update task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,15 +5,7 @@
 class Rectangle:
     """Class rectangle that define a rectangle"""
     def __init__(self, width=0, height=0):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         self.width = width
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.height = height
 
     def area(self):
