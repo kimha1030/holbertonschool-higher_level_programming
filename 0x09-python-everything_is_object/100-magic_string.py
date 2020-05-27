2aeb035683ea4c3e444d1b249ab874f074e25311
kimha1030 <1478@holbertonschool.com>
2020-05-26 20:35:10 -0500

    Update task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(list_a=[]):
     list_a.append("Holberton")
-    return (' '.join([str(word) for word in list_a]))
+    return (', '.join([str(word) for word in list_a]))
