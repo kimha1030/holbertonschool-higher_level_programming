f43f5fa327041241153d01d4a8e80e53fdf8d5d3
kimha1030 <1478@holbertonschool.com>
2020-05-27 23:24:22 -0500

    Advanced Task 101

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module to find the product o f two matrix
+"""
+import numpy
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function to find the product of two matrix
+    """
+    msg1 = "m_a should contain only integers or floats"
+    msg2 = "m_b should contain only integers or floats"
+    msg3 = "each row of m_a must be of the same size"
+    msg4 = "each row of m_b must be of the same size"
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    if len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        if type(row) != list:
+            raise TypeError("m_a must be a list of lists")
+        if len(row) == 0:
+            raise TypeError("m_a can't be empty")
+        if len(row) != len(m_a[0]):
+            raise TypeError(msg3)
+        for c in row:
+            if type(c) not in (float, int):
+                raise TypeError(msg1)
+    for row in m_b:
+        if type(row) != list:
+            raise TypeError("m_b must be a list of lists")
+        if len(row) == 0:
+            raise TypeError("m_b can't be empty")
+        if len(row) != len(m_b[0]):
+            raise TypeError(msg4)
+        for c in row:
+            if type(c) not in (float, int):
+                raise TypeError(msg2)
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    multi = numpy.matmul(m_a, m_b)
+    return (multi)
