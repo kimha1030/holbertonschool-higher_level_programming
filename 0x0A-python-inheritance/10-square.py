afd2755594c4a7b6871b4bc5c14a7d9bf1a8da51
kimha1030 <1478@holbertonschool.com>
2020-06-02 18:56:33 -0500

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Method check value of name, age, distance"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Method check width and height of Class Rectangle """
+    def __init__(self, width, height):
+        BaseGeometry.__init__(self)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return area"""
+        area = self.__width * self.__height
+        return area
+
+    def __str__(self):
+        """method print with __str__"""
+        msge = "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        return (msge)
+
+
+class Square(Rectangle):
+    """ Method check size of square """
+    def __init__(self, size):
+        Rectangle.__init__(self, size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return area"""
+        area = self.__size * self.__size
+        return area
