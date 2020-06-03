2b478e08c5a8a8b77c2a2919de7f4a553a8e6396
kimha1030 <1478@holbertonschool.com>
2020-06-02 19:37:50 -0500

    Update task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Function that compares if is instance"""
+
+
 def inherits_from(obj, a_class):
+    """Function compares obj and a_class"""
     if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     else:
