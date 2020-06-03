9580b66196d02364763839bf6d42eafbd52b9922
kimha1030 <1478@holbertonschool.com>
2020-06-03 18:13:30 -0500

    Task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that allows returns the dictionary description """
+
+
+def class_to_json(obj):
+    """Function: returns the dictionary description """
+    return(obj.__dict__)
