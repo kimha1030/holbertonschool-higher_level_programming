782058a5bd5dea759a0c14ae498e668254cb9c3c
kimha1030 <1478@holbertonschool.com>
2020-06-03 17:49:20 -0500

    Task 5

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that allows read a number of lines of file"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function: read a number of lines of file"""
+    return (json.dumps(my_obj))
