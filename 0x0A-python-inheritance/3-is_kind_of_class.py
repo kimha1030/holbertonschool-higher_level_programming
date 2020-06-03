c699fae66ec9daeb663d818d3a8e3b25d80c607b
kimha1030 <1478@holbertonschool.com>
2020-06-02 19:39:45 -0500

    Update task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Function that compares if obj is instance of class"""
+
+
 def is_kind_of_class(obj, a_class):
+    """Function: compares obj and class"""
     if isinstance(obj, a_class):
         return True
     else:
