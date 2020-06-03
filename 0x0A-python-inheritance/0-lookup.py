b2ac6bccd71f72b55fec61f11ca97cb71afe86ff
kimha1030 <1478@holbertonschool.com>
2020-06-02 19:33:03 -0500

    Update task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Function that print the attributes of object"""
+
 
 def lookup(obj):
     """Function that return the available attributes"""
     list_o = []
-    list_o = dir(obj)
+    list_o = getattr(obj)
     return list_o
