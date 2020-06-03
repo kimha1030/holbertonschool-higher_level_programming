dc78c9a7557ec9a054f380ee825dc84e085473c0
kimha1030 <1478@holbertonschool.com>
2020-06-02 19:35:57 -0500

    Update task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Function that shows if type obj is equal to class"""
+
+
 def is_same_class(obj, a_class):
+    """Function: compares obj and class"""
     if type(obj) is a_class:
         return True
     else:
