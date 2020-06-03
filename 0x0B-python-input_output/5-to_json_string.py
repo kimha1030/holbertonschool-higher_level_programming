d4102817b3c667a16e4acea1814e10479caaa7cc
kimha1030 <1478@holbertonschool.com>
2020-06-03 17:51:53 -0500

    Task 5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Function that allows convert obj json to string"""
+"""Function that allows convert to obj json from string"""
 import json
 
 
 def to_json_string(my_obj):
-    """Function: convert obj json to string""""
+    """Function: convert to obj json from str"""
     return (json.dumps(my_obj))
