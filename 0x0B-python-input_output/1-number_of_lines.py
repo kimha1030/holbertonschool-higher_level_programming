2c432006e87fc188f967454c8611060950a7fab6
kimha1030 <1478@holbertonschool.com>
2020-06-03 18:02:18 -0500

    Update task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,6 +5,7 @@
 def number_of_lines(filename=""):
     """Function: count number of lines of file"""
     nb_line = 0
-    for line in open(filename):
-        nb_line = nb_line + 1
+    with open(filename, "r") as my_file:
+        for line in my_file:
+            nb_line = nb_line + 1
     return (nb_line)
