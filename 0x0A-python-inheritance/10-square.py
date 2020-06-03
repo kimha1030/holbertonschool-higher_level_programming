12cec2d40e6bf3a81278db52dd43071387987b31
kimha1030 <1478@holbertonschool.com>
2020-06-02 21:54:18 -0500

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Method check size of square """
     def __init__(self, size):
+        """Constructor Method"""
         super().__init__(size, size)
         super().integer_validator("size", size)
         self.__size = size
