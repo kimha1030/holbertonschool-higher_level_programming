182db5be4f2c21f39bbfd5f1858bb137a261a4fd
kimha1030 <1478@holbertonschool.com>
2020-06-03 17:50:18 -0500

    Task 5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Function that allows read a number of lines of file"""
+"""Function that allows convert obj json to string"""
 import json
 
 
 def to_json_string(my_obj):
-    """Function: read a number of lines of file"""
+    """Function: convert obj json to string""""
     return (json.dumps(my_obj))
