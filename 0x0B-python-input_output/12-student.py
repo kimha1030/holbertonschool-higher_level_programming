ae9c790beb3c46a4c359d3cb561dd36836f96ca5
kimha1030 <1478@holbertonschool.com>
2020-06-03 20:09:26 -0500

    Task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Function: returns the dictionary with the property of class"""
+        if type(attrs) == list:
+            for s in attrs:
+                if type(s) is not str:
+                    return(self.__dict__)
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                for k in attrs:
+                    if k == key:
+                        new_dict[key] = value
+            return(new_dict)
+        else:
+            return(self.__dict__)
