b48d9653ed01be6244706e5cbefe28e09bb87a8f
kimha1030 <1478@holbertonschool.com>
2020-06-04 22:39:27 -0500

    Update task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,4 +10,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    print("my name is {:s} {:s}".format(first_name, last_name, end=""))
+    print("My name is " + first_name + last_name))
