a14175df42e35ae554b083e4eb28c1a80e9572b9
kimha1030 <1478@holbertonschool.com>
2020-06-04 23:03:00 -0500

    Update task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,24 +8,25 @@ def matrix_divided(matrix, div):
     """
     msg1 = "matrix must be a matrix (list of lists) of integers/floats"
     msg2 = "Each row of the matrix must have the same size"
-    if type(div) not in (float, int):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if len(matrix) == 0:
+    if matrix == None and div == None:
         raise TypeError(msg1)
-    if type(matrix) is not list:
-        raise TypeError(msg1)
-    x = []
-    for row in matrix:
-        if type(row) != list:
+    else:
+        if type(div) not in (float, int):
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+        if type(matrix) is not list:
             raise TypeError(msg1)
-        if len(row) == 0:
-            raise TypeError(msg1)
-        if len(row) != len(matrix[0]):
-            raise TypeError(msg2)
-        for c in row:
-            if type(c) not in (float, int):
+        x = []
+        for row in matrix:
+            if type(row) != list:
                 raise TypeError(msg1)
-    x = [[round(c/div, 2) for c in row] for row in matrix]
-    return (x)
+            if len(row) == 0:
+                raise TypeError(msg1)
+            if len(row) != len(matrix[0]):
+                raise TypeError(msg2)
+            for c in row:
+                if type(c) not in (float, int):
+                    raise TypeError(msg1)
+        x = [[round(c/div, 2) for c in row] for row in matrix]
+        return (x)
