f27e55de1dc43966a8f9b1fca05ac76886683757
kimha1030 <1478@holbertonschool.com>
2020-06-05 21:59:37 -0500

    Update task 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
     triangle = [[1]]
     for r in range(1, n):
         val_r = [1]
-        for c in range (1, r):
+        for c in range(1, r):
             val_r.append(triangle[r-1][c-1] + triangle[r-1][c])
         val_end = 1
         val_r.append(val_end)
