393da206b7a97a5c215f72a40fb189bbbc13d2a7
kimha1030 <1478@holbertonschool.com>
2020-06-09 15:52:43 -0500

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,21 +7,19 @@ class Square(Rectangle):
     """Method check width, height, x and y of Class Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Method constructor"""
-        self.size = size
         super().__init__(size, size, x, y, id)
-        self.__x = self.x
-        self.__y = self.y
+        self.size = size
 
     def __str__(self):
         """method print with __str__"""
         message_str = "(Square) ({}) {}/{} - {}".format(
-            self.id, self.__x, self.__y, self.__size)
+            self.id, self.x, self.y, self.size)
         return message_str
 
     @property
     def size(self):
         """Return protected width"""
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -31,12 +29,12 @@ class Square(Rectangle):
         if value <= 0:
             raise ValueError("width must be > 0")
         else:
-            self.__size = value
+            self.width = value
+            self.height = value
 
     def update(self, *args, **kwargs):
-        """Method update"""
+        list_arg = ["id", "size", "x", "y"]
         if args and args is not None:
-            list_arg = ["id", "width", "height", "x", "y"]
             for x in range(len(list_arg)):
                 if x < len(args):
                     setattr(self, list_arg[x], args[x])
@@ -45,7 +43,6 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """Method: convert to dictionary"""
         list_attr = ["id", "size", "x", "y"]
         new_Dict = {}
         for x in range(len(list_attr)):
