54e7134e80af48879813f5f6d3993b6582ad5ef8
kimha1030 <1478@holbertonschool.com>
2020-06-09 17:53:20 -0500

    Update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,10 +2,18 @@
 """Unittest for Base
 """
 import unittest
+import pep8
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """Test for Base"""
+    def test_base_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/base.py'])
+        self.assertEqual(result.total_errors, 0)
+
     def test_none(self):
         base_1 = Base()
         self.assertEqual(base_1.id, 1)
