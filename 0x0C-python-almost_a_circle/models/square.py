babe66d2fdb936c9d2a002af4c5d21a8ec220bbd
kimha1030 <1478@holbertonschool.com>
2020-06-09 15:42:10 -0500

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,28 +9,34 @@ class Square(Rectangle):
         """Method constructor"""
         super().__init__(size, size, x, y, id)
         self.size = size
+        self.__x = self.x
+        self.__y = self.y
 
     def __str__(self):
-        """Method print with __str__"""
+        """method print with __str__"""
         message_str = "(Square) ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.size)
+            self.id, self.__x, self.__y, self.__size)
         return message_str
 
     @property
     def size(self):
         """Return protected width"""
-        return self.width
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """Modify the value of width or height as size"""
-        self.width = value
-        self.height = value
+        """Modifiy size as value"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__size = value
 
     def update(self, *args, **kwargs):
-        """Method: update arguments"""
-        list_arg = ["id", "size", "x", "y"]
+        """Method update"""
         if args and args is not None:
+            list_arg = ["id", "width", "height", "x", "y"]
             for x in range(len(list_arg)):
                 if x < len(args):
                     setattr(self, list_arg[x], args[x])
