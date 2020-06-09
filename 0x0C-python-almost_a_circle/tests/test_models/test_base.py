e58cb3ede096623959c9c4eec9c11c3f2e5d8df9
kimha1030 <1478@holbertonschool.com>
2020-06-09 18:21:30 -0500

    Update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -15,31 +15,38 @@ class TestBase(unittest.TestCase):
         self.assertEqual(result.total_errors, 0)
 
     def test_none(self):
+        """Test none"""
         base_1 = Base()
         self.assertEqual(base_1.id, 1)
         base_10 = Base()
         self.assertEqual(base_10.id, 2)
 
     def test_integer(self):
+        """Test integer"""
         base_2 = Base(20)
         self.assertEqual(base_2.id, 20)
 
     def test_float(self):
+        """Test float"""
         base_3 = Base(4.3)
         self.assertEqual(base_3.id, 4.3)
 
     def test_string(self):
+        """Test string"""
         base_4 = Base("Hola")
         self.assertEqual(base_4.id, "Hola")
 
     def test_dict(self):
+        """Test dict"""
         base_5 = Base({"Name", "Jhon"})
         self.assertEqual(base_5.id, {"Name", "Jhon"})
 
     def test_tuple(self):
+        """Test tuple"""
         base_6 = Base((1, 2))
         self.assertEqual(base_6.id, (1, 2))
 
     def test_list(self):
+        """Test list"""
         base_7 = Base([1, 2, 3])
         self.assertEqual(base_7.id, [1, 2, 3])
