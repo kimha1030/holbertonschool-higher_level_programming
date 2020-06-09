4a62c2e802e30cdaf36be66e1c7474686740e5e0
kimha1030 <1478@holbertonschool.com>
2020-06-09 16:17:22 -0500

    Update rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,10 +2,18 @@
 """Unittest for Rectangle
 """
 import unittest
+import pep8
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
+    """Test for Rectangle"""
+    def test_rectangle_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0)
+
     def test_pos_width(self):
         rect_1 = Rectangle(1, 2, 3, 4)
         self.assertEqual(rect_1.width, 1)
