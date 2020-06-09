d00059246cdddc8e3230737ab09eacb277295211
kimha1030 <1478@holbertonschool.com>
2020-06-09 17:31:06 -0500

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,11 +18,10 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Function: convert to obj json from str"""
-        if type(list_dictionaries) is list:
-            if list_dictionaries is None or len(list_dictionaries) == 0:
-                return []
-            else:
-                return (json.dumps(list_dictionaries))
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
