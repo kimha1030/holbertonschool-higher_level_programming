fdfa50a6d186a63ab1b765a3f7d959306d40daeb
kimha1030 <1478@holbertonschool.com>
2020-07-14 17:23:47 +0000

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Task 1
 -- If the user exists, delete and then, create new user with all privileges on localhost.
-DROP USER 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON * . * TO 'user_0d_1'@'localhost';
