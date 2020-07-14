cb2168cf362dea8a1ab6f9e7f81bf20a34918814
kimha1030 <1478@holbertonschool.com>
2020-07-13 22:29:20 -0500

    Add task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Task 102
 -- Top 3 of cities temperature between July and August
-SELECT FIRST 3 city, AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 1 AND 8 GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 1 AND 8 GROUP BY city LIMIT 3 ORDER BY avg_temp DESC;
