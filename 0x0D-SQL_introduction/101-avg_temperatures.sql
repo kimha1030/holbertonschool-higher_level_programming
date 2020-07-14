f92975a16785440098999fe1842d2b54446779a0
kimha1030 <1478@holbertonschool.com>
2020-07-13 21:33:31 -0500

    Add task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Task 101
 -- Import table in DB
-SELECT 'city', AVG('value') AS 'avg_temp' FROM 'temperatures' ORDER BY 'temperature' DESC; 
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY value DESC; 
