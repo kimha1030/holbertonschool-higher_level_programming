a1aa581905a8a59d7aaa5e737249d0b294036ddb
kimha1030 <1478@holbertonschool.com>
2020-07-14 22:15:51 +0000

    Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Task 13
 -- List name of geners and count
-SELECT tv_genres.name, COUNT(tv_shows.id) AS number_of_shows FROM tv_shows 
+SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows FROM tv_shows 
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
