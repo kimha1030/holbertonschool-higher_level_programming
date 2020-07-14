f8210959f1ce02268a75caf29edcdf9728eedd5e
kimha1030 <1478@holbertonschool.com>
2020-07-14 17:34:13 +0000

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ DROP DATABASE IF EXISTS hbtn_0d_2;
 CREATE DATABASE hbtn_0d_2;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2. * TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
