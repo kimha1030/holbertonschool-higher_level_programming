3618ed42252649602995e30a9f0b7a2127c359b2
kimha1030 <1478@holbertonschool.com>
2020-07-16 00:22:28 -0500

    Task 102 and Task 103

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- Task 102
--- List name of geners by rating
+-- List all titles of te shows by rating
 SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings
 INNER JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Task 103
+-- List name of geners by rating
+SELECT tv_genres.name AS name, SUM(tv_show_ratings.rate) AS rating 
+FROM tv_show_ratings
+INNER JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id
+INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY name
+ORDER BY rating DESC;
