b0f7171543b2aa1af6840d38fa731a5889d84921
kimha1030 <1478@holbertonschool.com>
2020-08-08 03:33:54 +0000

    Update task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,11 @@ import sys
 if __name__=='__main__':
 	con = mdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
 	cur = con.cursor()
-	cur.execute("SELECT * FROM states")
+	sql = """SELECT * FROM states ORDER BY states.id ASC"""
+	cur.execute(sql)
 	rows = cur.fetchall()
 	for row in rows:
 		print(row)
+	cur.close()
+	con.close()
 
