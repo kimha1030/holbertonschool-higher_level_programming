79a8f12718d0ffc05240805d2fde27365f3cf3fb
kimha1030 <1478@holbertonschool.com>
2020-08-08 13:16:18 -0500

    Update task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-#Lists all states from DB hbtn_0e_0_usa
+# Lists all states from DB hbtn_0e_0_usa
 import MySQLdb as mdb
 import sys
 
+
+def select_states():
+    """Function select_states"""
+    mdb.connect(
+        user=sys.argv[1],
+        port=3306,
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = con.cursor()
+    sql = """SELECT * FROM states ORDER BY states.id ASC"""
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
+
+
 if __name__ == '__main__':
-	con = mdb.connect(user=sys.argv[1], port=3306,
-		passwd=sys.argv[2], db=sys.argv[3])
-	cur = con.cursor()
-	sql = """SELECT * FROM states ORDER BY states.id ASC"""
-	cur.execute(sql)
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
-	cur.close()
-	con.close()
+    select_states()
