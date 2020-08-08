c0c5a154c5bf49502855c3a3e88550f8b651799c
kimha1030 <1478@holbertonschool.com>
2020-08-08 21:14:48 +0000

    Update task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-#Lists all states from DB where name is arg order by name, safe from MySQL injections!
-import MySQLdb as mdb
+"""
+Lists all states from DB, of safe way from MySQL injections
+"""
+import MySQLdb
 import sys
 
-if __name__=='__main__':
-	con = mdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
-	cur = con.cursor()
-	sql = """SELECT * FROM states WHERE BINARY name = '{:s}' ORDER BY states.id ASC""".format(sys.argv[4])
-	cur.execute(sql)
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
-	cur.close()
-	con.close()
 
+def filter_states_safe():
+    """Select name according input"""
+    con = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        port=3306,
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8")
+    cur = con.cursor()
+    sql = """SELECT * FROM states WHERE BINARY name = '{:s}'\
+             ORDER BY states.id ASC""".format(sys.argv[4])
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
+
+if __name__ == '__main__':
+    filter_states_safe()
