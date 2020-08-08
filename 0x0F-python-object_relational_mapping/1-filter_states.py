f718f420877415e04de45810a4a4fde30b8f977b
kimha1030 <1478@holbertonschool.com>
2020-08-08 03:34:30 +0000

    Update task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,8 +6,11 @@ import sys
 if __name__=='__main__':
 	con = mdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
 	cur = con.cursor()
-	cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+	sql = """SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC"""
+	cur.execute(sql)
 	rows = cur.fetchall()
 	for row in rows:
 		print(row)
+	cur.close()
+	con.close()
 
