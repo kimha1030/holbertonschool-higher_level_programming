d0ede843b15066e6a1896cd5c1e6514b6331471d
kimha1030 <1478@holbertonschool.com>
2020-08-08 21:47:07 +0000

    Update task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-#Lists all cities and theirs states
-import MySQLdb as mdb
+"""
+Lists all cities according states
+"""
+import MySQLdb
 import sys
 
-if __name__=='__main__':
-	con = mdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
-	cur = con.cursor()
-	sql = """SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"""
-	cur.execute(sql)
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
-	cur.close()
-	con.close()
 
+def filter_states_cities():
+    """List cities according states"""
+    con = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        port=3306,
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8")
+    cur = con.cursor()
+    sql = """SELECT cities.id, cities.name, states.name\
+             FROM cities JOIN states\
+             ON cities.state_id = states.id\
+             ORDER BY cities.id ASC"""
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
+
+if __name__ == '__main__':
+    filter_states_cities()
