cdab0cd80778fa26be9eb1d6951f26edfe264766
kimha1030 <1478@holbertonschool.com>
2020-08-08 03:36:32 +0000

    Update task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ import sys
 if __name__=='__main__':
 	con = mdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
 	cur = con.cursor()
-	cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id ASC;".format(sys.argv[4]))
+	sql = """SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC""".format(sys.argv[4])
+	cur.execute(sql)
 	rows = cur.fetchall()
 	for row in rows:
 		print(row)
