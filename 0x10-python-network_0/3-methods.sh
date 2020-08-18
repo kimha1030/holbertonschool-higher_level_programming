48b28625ba433752c7b438a1b17bc2077d9543de
kimha1030 <1478@holbertonschool.com>
2020-08-18 15:02:26 -0500

    Update Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Task 2: Allowed Methods
-curl -X OPTIONS -s "$1" -i | grep Allow | awk '{print $2, $3, $4}'
+# Task 3: Allowed Methods
+curl -X OPTIONS -s "$1" -i | grep Allow | cut -d: -f2-4
