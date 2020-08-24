77a9059142edae2b553ae7b5ea41077c21d53749
kimha1030 <1478@holbertonschool.com>
2020-08-24 12:45:10 -0500

    Update task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,14 @@
 from urllib import request
 from sys import argv
 
-with request.urlopen(argv[1]) as response:
-    html = response.read()
-    print(response.headers.get("x-request-id"))
+
+def response_header():
+    """Takes a url, send request and display value"""
+    url_request = argv[1]
+    with request.urlopen(url_request) as response:
+        html = response.read()
+        print(response.headers.get("X-Request-Id"))
+
+
+if __name__ == "__main__":
+    response_header()
