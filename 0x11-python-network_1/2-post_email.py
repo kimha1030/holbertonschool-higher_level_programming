534f96e9134e3a01763a10c4e0e5ff0dfad0e308
kimha1030 <1478@holbertonschool.com>
2020-08-24 12:49:39 -0500

    Update task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,13 +4,19 @@ from urllib import request
 from urllib import parse
 from sys import argv
 
-post_url = argv[1]
-params = {
-    'email': argv[2]
-}
-query_string = parse.urlencode(params)
-post_data = query_string.encode("ascii")
 
-with request.urlopen(post_url, post_data) as post_response:
-    response_text = post_response.read()
-    print(response_text.decode("UTF-8"))
+def post_request():
+    """Takes a url and email, sends POST request and display body"""
+    post_url = argv[1]
+    params = {
+        'email': argv[2]
+    }
+    query_string = parse.urlencode(params)
+    post_data = query_string.encode("ascii")
+    with request.urlopen(post_url, post_data) as post_response:
+        response_text = post_response.read()
+        print(response_text.decode("UTF-8"))
+
+
+if __name__ == "__main__":
+    post_request()
