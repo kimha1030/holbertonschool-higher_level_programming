5b35e711f8122bd9896eb5e1c9c81664021195f0
kimha1030 <1478@holbertonschool.com>
2020-08-24 12:37:56 -0500

    Update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """Task 0: What's my status? #0"""
-from urllib import request
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body request:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf-8: {}".format(html.decode("UTF-8")))
+import urllib.request
+
+
+def status():
+    """Check the status of URL"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body request:")
+        print("\t- type: {:}".format(type(html)))
+        print("\t- content: {:}".format(html))
+        print("\t- utf-8: {:}".format(html.decode("UTF-8")))
+
+
+if __name__ == "__main__":
+    status()
