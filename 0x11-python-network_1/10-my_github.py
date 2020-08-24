196d9e1d173cdb9bb476f2a47eccda363584f884
kimha1030 <1478@holbertonschool.com>
2020-08-24 18:11:35 -0500

    Add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Task 9: My Github!"""
 import requests
-from requests.auth import HTTPBasicAuth
 from sys import argv
 
 
@@ -11,7 +10,7 @@ def my_github():
     password = argv[2]
     url = 'https://api.github.com/users/' + username
     response = requests.get(
-        url, ",", auth=HTTPBasicAuth(username, password))
+        url, ",", auth=(username, password))
     response_to_json = response.json()
     id_user = response_to_json.get('id')
     if id_user != "None":
