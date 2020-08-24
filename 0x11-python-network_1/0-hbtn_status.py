2437c581e135219a9ecb5eec6d7b0b471455f91c
kimha1030 <1478@holbertonschool.com>
2020-08-24 12:41:05 -0500

    Update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,13 @@ import urllib.request
 
 def status():
     """Check the status of URL"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         html = response.read()
-        print("Body request:")
+        print("Body response:")
         print("\t- type: {:}".format(type(html)))
         print("\t- content: {:}".format(html))
-        print("\t- utf-8: {:}".format(html.decode("UTF-8")))
+        print("\t- utf8 content: {:}".format(html.decode("UTF-8")))
 
 
 if __name__ == "__main__":
