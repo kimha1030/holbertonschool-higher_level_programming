4da2212a1b61f79c7308271c848d8cd8c904163f
kimha1030 <1478@holbertonschool.com>
2020-08-24 17:59:37 -0500

    Add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ def my_github():
     """Takes a url, sendsrequest and display body"""
     username = argv[1]
     password = argv[2]
-    url = 'https://api.github.com/users'
+    url = 'https://api.github.com/users/' + username
     response = requests.get(url, ",", auth=HTTPBasicAuth(username, password))
     print(response.json().get('id'))
 
