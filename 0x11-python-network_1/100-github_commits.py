adacf745775d494c3305ed1c3d70beb14fe3d3af
kimha1030 <1478@holbertonschool.com>
2020-08-24 21:48:05 -0500

    Add task advanced 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,17 +4,18 @@ import requests
 from sys import argv
 
 
-def my_github1():
+def my_github():
     """Takes a url, sendsrequest and display body"""
     repo = argv[1]
     owner = argv[2]
-    url = 'https://api.github.com/repos/' + owner + '/' + repo + '/commits'
+    url = 'https://api.github.com/repos/' + \
+        owner + '/' + repo + '/commits?per_page=10'
     response = requests.get(url)
     response_data = response.json()
-    for i in range(10):
+    for i in range(len(response_data)):
         print(response_data[i]['sha'], response_data[i]
               ['commit']['author']['name'])
 
 
 if __name__ == "__main__":
-    my_github1()
+    my_github()
