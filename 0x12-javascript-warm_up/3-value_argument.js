3660de27c953ecac67d2e744b4cb57db6038be72
kimha1030 <1478@holbertonschool.com>
2020-09-15 12:20:16 -0500

    Update task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Task 3. Value of my argument
 const myVar = process.argv.slice(2);
-if (myVar == '') {
+if (!myVar[0]) {
   console.log('No argument');
 } else {
   console.log(myVar[0]);
