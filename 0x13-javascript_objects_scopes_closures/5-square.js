81ea3b7ace1faf104ca04d37e5352f8b6b77dd43
kimha1030 <1478@holbertonschool.com>
2020-09-21 14:59:03 -0500

    Add task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Task 5. Square #0
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
