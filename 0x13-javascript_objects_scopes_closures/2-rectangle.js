1e4fff6dfe5f31d0548249a761405499b8aadd0a
kimha1030 <1478@holbertonschool.com>
2020-09-21 13:30:32 -0500

    Add task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Task 2. Rectangle #2
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
