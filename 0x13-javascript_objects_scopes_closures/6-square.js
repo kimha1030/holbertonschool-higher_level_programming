fe8d0a5cce55d921583fde68f9aa47725c91d45c
kimha1030 <1478@holbertonschool.com>
2020-09-21 15:39:40 -0500

    Add task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,16 +6,13 @@ const parSquare = require('./5-square');
 class Square extends parSquare {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c) {
-      const myWidth = parseInt(this.size);
-      const myHeight = parseInt(this.size);
       const pattern = c;
-      for (let j = 0; j < myHeight; j++) {
-        console.log(pattern.repeat(myWidth));
+      for (let j = 0; j < this.width; j++) {
+        console.log(pattern.repeat(this.width));
       }
     } else {
       this.print();
