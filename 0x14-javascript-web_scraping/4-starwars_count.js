f10df6b1743e3fffd7c29168644762b840342d7c
kimha1030 <1478@holbertonschool.com>
2020-09-22 15:27:13 -0500

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Task 4. Star wars Wedge Antilles
+const urlMovie = process.argv.slice(2, 3);
+const request = require('request');
+request(urlMovie[0], function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+  const results = JSON.parse(body).results;
+  let count = 0;
+  for (const i in results) {
+    for (const j in results[i].characters) {
+      if (results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') { count = count + 1; }
+    }
+  }
+  console.log(count);
+});
