70a813dd641e52da9055ff277866bd5dba9baba5
kimha1030 <1478@holbertonschool.com>
2020-09-22 12:11:36 -0500

    Update file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Task 0. Readme
-var nameFile = process.argv.slice(2, 3);
-var fs = require('fs');
+const nameFile = process.argv.slice(2, 3);
+const fs = require('fs');
 fs.readFile(nameFile[0], 'utf-8', function (err, data) {
   if (err) {
     return console.log(err);
