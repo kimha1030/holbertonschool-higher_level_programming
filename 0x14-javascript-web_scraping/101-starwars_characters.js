e183964c1cf32146c9de83223241eb3d0010e8c2
kimha1030 <1478@holbertonschool.com>
2020-09-22 23:49:08 -0500

    Add task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 // Task 8. Right order
-const idMovie = process.argv.slice(2, 3);
-const urlMovie = 'https://swapi-api.hbtn.io/api/films/' + idMovie[0];
-const request = require('request');
-request(urlMovie, function (err, response, body) {
-  if (err) {
-    return console.log(err);
-  }
-  const results = JSON.parse(body).characters;
-  for (const i in results) {
-    request(results[i], function (err, response, body) {
-      if (err) {
-        return console.log(err);
+
+for (let x = 0; x < 10; x++) {
+  const urlMovie = 'https://swapi-api.hbtn.io/api/people/?page=' + x;
+  const idMovie = process.argv.slice(2, 3);
+  const request = require('request');
+  request(urlMovie, function (err, response, body) {
+    if (err) {
+      return console.log(err);
+    }
+    const results = JSON.parse(body).results;
+    for (const i in results) {
+      for (const j in results[i].films) {
+        if (results[i].films[j].includes(idMovie[0])) {
+          console.log(results[i].name);
+        }
       }
-      console.log(JSON.parse(body).name);
-    });
-  }
-});
+    }
+  });
+}
